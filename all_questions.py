9e22f0c06088d8498ef0479f883c4636735ad7fb
Daniel <drw22a@fsu.edu>
2024-03-25 13:43:16 -0400

    current progress for hw4

--- all_questions.py
+++ all_questions.py
@@ -8,22 +8,22 @@ def question1():
     answers = {}
 
     # string "yes" or "no"
-    answers["(a)"] = None
-    answers["(b)"] = None
-    answers["(c)"] = None
-    answers["(d)"] = None
+    answers["(a)"] = "no"
+    answers["(b)"] = "no"
+    answers["(c)"] = "yes"
+    answers["(d)"] = "yes"
 
     # explain-string: explanation in english prose
-    answers["(a) explain"] = None
-    answers["(b) explain"] = None
-    answers["(c) explain"] = None
-    answers["(d) explain"] = None
+    answers["(a) explain"] = "The ruleset is not mutually exclusive since some cases can satisfy multiple rules at once, such as a low income person with a house (rules 1 and 3)"
+    answers["(b) explain"] = "The ruleset does not cover every possible scenario from the attributes."
+    answers["(c) explain"] = "The rules should be ordered, since the rules are not mutually exclusive."
+    answers["(d) explain"] = "There should be a default rule, since the ruleset does not cover every posisble scenario."
 
     return answers
 
 
 # -----------------------------------------------------------
-def question2():
+def question2():#told that this question is not necessary by TA.
     answers = {}
 
     # string "yes" or "no"
@@ -44,14 +44,14 @@ def question3():
     answers = {}
 
     # string "yes" or "no"
-    answers["(a)"] = None
-    answers["(b)"] = None
-    answers["(c)"] = None
+    answers["(a)"] = "yes"
+    answers["(b)"] = "no"
+    answers["(c)"] = "no"
 
     # explain-string: explanation in english prose
-    answers["(a) example"] = None
-    answers["(b) example"] = None
-    answers["(c) example"] = None
+    answers["(a) example"] = "Yes, since each rule produces a different results and the results cannot overlap."
+    answers["(b) example"] = "No, since there are no rules covering amphibians."
+    answers["(c) example"] = "No, since the rules are mutually exclusive the order in which they are enforced does not matter."
 
     return answers
 # -----------------------------------------------------------
@@ -59,16 +59,16 @@ def question7():
     answers = {}
 
     # bool: True/False
-    answers["(a)"] = None
-    answers["(b)"] = None
-    answers["(c)"] = None
-    answers["(d)"] = None
+    answers["(a)"] = True
+    answers["(b)"] = True
+    answers["(c)"] = False
+    answers["(d)"] = True
 
     # explain_string: explanation in english prose
-    answers["(a) explain"] = None
-    answers["(b) explain"] = None
-    answers["(c) explain"] = None
-    answers["(d) explain"] = None
+    answers["(a) explain"] = "During the forward pass of Back-Propagation, the k + 1th layer is calculated using the activations at nodes in the kth layer."
+    answers["(b) explain"] = "During the forward pass of an ANN, the activations at the k + 1th layer are calucluated based off of the weights and biases of the previous layer, regardless of if the model is testing or training values."
+    answers["(c) explain"] = "While the Vanishing Gradient Problem does cause gradients to slowly 'vanish' into values of zero, the test errors remaining large is not indicative of the issue."
+    answers["(d) explain"] = "If all of the training instances were to be perfectly classified, the predicted output would always match the actual ouput, meaning that the loss function will result in a value of zero."
 
     return answers
 
@@ -183,9 +183,9 @@ def question10():
 if __name__ == '__main__':
     answers_dict = {}
     answers_dict['question1'] = question1()
-    answers_dict['question2'] = question2()
+    #answers_dict['question2'] = question2()
     answers_dict['question3'] = question3()
-    answers_dict['question4'] = question4()
+    #answers_dict['question4'] = question4()
     answers_dict['question7'] = question7()
     answers_dict['question8'] = question8()
     answers_dict['question9'] = question9()
